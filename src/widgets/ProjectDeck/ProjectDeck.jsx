9e6c214acefeee6e593441dff4b07b2c734910f5
adamcesco <acescobedo@smu.edu>
2024-09-04 00:44:59 -0500

    Refactor ProjectDeck component to display ProjectCards in groups of two, where every two ProjectCards get's placed in their own project-deck class div

--- src/widgets/ProjectDeck/ProjectDeck.jsx
+++ src/widgets/ProjectDeck/ProjectDeck.jsx
@@ -8,7 +8,29 @@ import "./ProjectDeck.css";
 export default function ProjectDeck({ children }) {
   // todo: map 2 children at a time into flexbox rows, alternating sizes and grow directions
   // todo: make project-dect flex width grow when a project-card is clicked so that project cards do not shrink or wrap
-  return <div className="project-deck">{children}</div>;
+  //! not checked for odd number of children
+  const pairs = children.reduce((acc, child, index) => {
+    if (index % 2 === 0 && index === children.length - 1) {
+      acc.push([child, null]);
+    } else if (index % 2 === 0) {
+      acc.push([child]);
+    } else {
+      acc[acc.length - 1].push(child);
+    }
+    return acc;
+  }, []);
+
+  return pairs.map((pair, index) => (
+    <div
+      key={index}
+      className={`project-deck 
+          ${index % 2 === 1 ? "right-card-clicked" : ""}
+        }`}
+    >
+      {pair[0]}
+      {pair[1]}
+    </div>
+  ));
 }
 ProjectDeck.propTypes = {
   children: propTypes.node.isRequired,
