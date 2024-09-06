5f1e6d895ff15c079927557a800b4921beadd182
adamcesco <acescobedo@smu.edu>
2024-09-06 06:03:39 -0500

    Edit navbar-header__name-banner sizing mechanics. Add transition and color fade to navbar-header__menu-button

--- src/widgets/NavBarHeader/NavBarHeader.jsx
+++ src/widgets/NavBarHeader/NavBarHeader.jsx
@@ -4,38 +4,66 @@ import { Button } from "@/components/ui/button";
 
 import "./NavBarHeader.css";
 
-function NavBarHeader() {
-  return (
-    <>
-      <header className="navbar-header">
-        <div className="navbar-header__name-banner">
-          <p>JACQUELINE</p>
-          <p>&nbsp;ALEMAN</p>
+class NavBarHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mouseIsOn: "",
+    };
+  }
+
+  render() {
+    const { mouseIsOn } = this.state;
+    return (
+      <>
+        <header className="navbar-header">
+          <div className="navbar-header__name-banner">
+            <p>JACQUELINE</p>
+            <p>&nbsp;ALEMAN</p>
+          </div>
+        </header>
+        <div className="navbar-header__nav">
+          <a className="navbar-header__contact" href="/contactme">
+            Contact me!
+          </a>
+          <span className="navbar-header__menu">
+            <Button
+              variant="ghost"
+              className={`navbar-header__menu-button ${mouseIsOn !== "" && mouseIsOn !== "projects" ? "not-hover" : ""}`}
+              onMouseEnter={() => this.setState({ mouseIsOn: "projects" })}
+              onMouseLeave={() => this.setState({ mouseIsOn: "" })}
+            >
+              Projects
+            </Button>
+            &nbsp;*&nbsp;
+            <Button
+              variant="ghost"
+              className={`navbar-header__menu-button ${mouseIsOn !== "" && mouseIsOn !== "resume" ? "not-hover" : ""}`}
+              onMouseEnter={() => this.setState({ mouseIsOn: "resume" })}
+              onMouseLeave={() => this.setState({ mouseIsOn: "" })}
+            >
+              Resume
+            </Button>
+            &nbsp;*&nbsp;
+            <Button
+              variant="ghost"
+              className={`navbar-header__menu-button ${mouseIsOn !== "" && mouseIsOn !== "about" ? "not-hover" : ""}`}
+              onMouseEnter={() => this.setState({ mouseIsOn: "about" })}
+              onMouseLeave={() => this.setState({ mouseIsOn: ""})}
+            >
+              About
+            </Button>
+          </span>
         </div>
-      </header>
-      <div className="navbar-header__nav">
-        <a className="navbar-header__contact" href="/contactme">
-          Contact me!
-        </a>
-        <span className="navbar-header__menu">
-          <Button variant="ghost" className="navbar-header__menu-button">
-            Projects
-          </Button>
-          &nbsp;*&nbsp;
-          <Button variant="ghost" className="navbar-header__menu-button">
-            Resume
-          </Button>
-          &nbsp;*&nbsp;
-          <Button variant="ghost" className="navbar-header__menu-button">
-            About
-          </Button>
-        </span>
-      </div>
-      <p className="navbar-header__welcome-message">
-        Texas A&M senior, studying<br />Mechanical Engineering
-      </p>
-    </>
-  );
+        <p className="navbar-header__welcome-message">
+          Texas A&M senior, studying
+          <br />
+          Mechanical Engineering
+        </p>
+      </>
+    );
+  }
 }
 
 export default NavBarHeader;
