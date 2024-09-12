dafef2a8b552354fba3c932fd851c275e0ac0e6e
adamcesco <acescobedo@smu.edu>
2024-09-11 23:46:06 -0500

    restyling welcome page

--- src/widgets/NavBarHeader/NavBarHeader.jsx
+++ src/widgets/NavBarHeader/NavBarHeader.jsx
@@ -15,53 +15,54 @@ const NavBarHeader = () => {
   return (
     <>
       <header className="navbar-header">
-        <div className="navbar-header__name-banner">
-          <p>JACQUELINE</p>
-          <p>&nbsp;ALEMAN</p>
+        <div className="navbar-header__nav">
+          <Button className="navbar-header__name">
+            <Link href="/contactme" legacyBehavior>
+              Jacqueline Aleman
+            </Link>
+          </Button>
+          <NavigationMenu>
+            <NavigationMenuList className="navbar-header__menu">
+              <NavigationMenuItem
+                variant="ghost"
+                className="navbar-header__menu-button"
+              >
+                <Link href="/Projects" legacyBehavior passHref>
+                  <NavigationMenuLink>Projects</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              &nbsp;*&nbsp;
+              <NavigationMenuItem
+                variant="ghost"
+                className="navbar-header__menu-button"
+              >
+                <Link href="/Resume" legacyBehavior passHref>
+                  <NavigationMenuLink>Resume</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              &nbsp;*&nbsp;
+              <NavigationMenuItem
+                variant="ghost"
+                className="navbar-header__menu-button"
+              >
+                <Link href="/About" legacyBehavior passHref>
+                  <NavigationMenuLink>About</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
       </header>
-      <div className="navbar-header__nav">
-        <Button className="navbar-header__contact-button">
-          <Link href="/contactme" legacyBehavior>
-            Contact me!
-          </Link>
-        </Button>
-        <NavigationMenu>
-          <NavigationMenuList className="navbar-header__menu">
-            <NavigationMenuItem
-              variant="ghost"
-              className="navbar-header__menu-button"
-            >
-              <Link href="/Projects" legacyBehavior passHref>
-                <NavigationMenuLink>Projects</NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            &nbsp;*&nbsp;
-            <NavigationMenuItem
-              variant="ghost"
-              className="navbar-header__menu-button"
-            >
-              <Link href="/Resume" legacyBehavior passHref>
-                <NavigationMenuLink>Resume</NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            &nbsp;*&nbsp;
-            <NavigationMenuItem
-              variant="ghost"
-              className="navbar-header__menu-button"
-            >
-              <Link href="/About" legacyBehavior passHref>
-                <NavigationMenuLink>About</NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+      <div className="navbar-header__welcome-page">
+        <div className="navbar-header__welcome-page__text">
+          <p className="navbar-header__welcome-page__text__primary">
+            Welcome to my portfolio!
+          </p>
+          <p className="navbar-header__welcome-page__text__secondary">
+            Welcome to my portfolio!
+          </p>
+        </div>
       </div>
-      <p className="navbar-header__welcome-message">
-        Texas A&M senior, studying
-        <br />
-        Mechanical Engineering
-      </p>
     </>
   );
 };
